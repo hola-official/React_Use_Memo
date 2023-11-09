afd297453a4ac10c70754dede40f9bca4242c5b2
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-09 10:40:51 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,8 @@ function App() {
     return n <= 1 ? n : fib(n - 1) + fib(n - 2);
   }, [])
 
+  const fibNumber = useMemo(() => fib(userInput), [userInput, fib])
+
   return (
     <main className="App">
       <div className="input">
