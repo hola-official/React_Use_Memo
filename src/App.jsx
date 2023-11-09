41675afd0bb060d069bcf882e71fb1790638e1a0
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-09 10:25:09 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import { useEffect, useState, useCallback } from "react"
+import { useEffect, useState, useCallback, useMemo } from "react"
+
+const getArray = () => {
+  for(let i = 0; i < 1000000000; i++) {
+
+  }
+  return ['Muhammed', 'fineBoy']
+}
 
 function App() {
 
   const [userInput, setUserInput] = useState("")
-  const [result, setResult] = useState(0)
-  const [num1] = useState(4)
-  const [num2] = useState(5)
+  const [randomInput, setRandomInput] = useState('')
 
-  const sum = useCallback(() => num1 + num2, [num1, num2])
-
-  const buildArray = useCallback(() => [num1, num2], [num1, num2])
-
-  useEffect(() => {
-    console.log(`New sum. Value: ${buildArray()}`);
-    setResult(buildArray())
-  }, [buildArray])
+  c
 
   return (
     <main className="App">
